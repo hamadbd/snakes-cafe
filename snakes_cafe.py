c8f45c7e54d032dfff9a9245e6e1034bfb0a5c5d
hamadbd <hamaddwairi0@gmail.com>
2022-06-04 20:32:17 +0300

    snacks cafe for ordering

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,4 +1,5 @@
-welcomeMsg = """*************************************
+menu = """
+**************************************
 **    Welcome to the Snakes Cafe!   **
 **    Please see our menu below.    **
 **
@@ -10,40 +11,72 @@ Wings
 Cookies
 Spring Rolls
 Entrees
-----------
+-------
 Salmon
 Steak
 Meat Tornado
 A Literal Garden
-Desserts 
-----------
+Desserts
+--------
 Ice Cream
 Cake
 Pie
 Drinks
-----------
+------
 Coffee
 Tea
 Unicorn Tears
 ***********************************
 ** What would you like to order? **
 ***********************************
+>
 """
-food = ["Wings", "Cookies", "Spring Rolls", "Salmon", "Steak", "Meat Tornado", "A Literal Garden", "Ice Cream", "Cake","Pie","Coffee","Tea","Unicorn Tears"]
-print(welcomeMsg)
+# Define a menu to valedate the order process to order only from the menu
+menuList = ["Wings", "Cookies", "Spring Rolls", "Salmon", "Steak", "Meat Tornado", "A Literal Garden", "Ice Cream", "Cake", "Pie","Coffee","Tea","Unicorn Tears"]
+
+def startOrder(order):
+    print("Order function executed")
+    if menuList.count(order) == 0:
+        print("Sorry, we don't have this item in the menu")
+    else:
+        cart = []
+        cart.append(order)
+        count = cart.count(order)
+        print(f"** {count} order of {order} have been added to your meal **")
+        # addingToOrder = input(" > ")
+        while order != "quit":
+            if order != "quit":
+                addingToOrder = input(" > ")
+                if(addingToOrder != "quit"):
+                    if menuList.count(addingToOrder) == 0:
+                        print("Sorry, we don't have this item in the menu")
+                    else:
+                        cart.append(addingToOrder)
+                        count = cart.count(addingToOrder)
+                        if count == 1:
+                            print(f"** {count} order of {addingToOrder} have been added to your meal **")
+                        else:
+                            print(f"** {count} orders of {addingToOrder} have been added to your meal **")
+                else:
+                    print(f"You have ordered the following:")
+                    for x in cart:
+                        print(x)
+                    break
+            else:
+                print(f"You have ordered the following:")
+                for x in cart:
+                    print(x)
+                break
 
 
-def orders():
-    count = 0
-    order = ""
-    while order != 'quit':
-        print("What would you like to order?")
-        order = input(">")
-        if order == 'quit':
-            break
-        count = count + 1
-        print(f'** {count} order of {order} have been added to your meal**')
+
+def intro():
+    print("intro function executed")
+    order = input(menu)
+    while order != "quit":
+        startOrder(order)
+        break
 
 
-orders()
+intro()
 
